6c99a9dff8564377e97209cafff432ae729f7554
acevedosharp <acevedosharp@hotmail.com>
2020-06-23 15:25:25 -0500

    Fixed missing @Service annotation

--- src/main/kotlin/com/acevedosharp/persistence_layer/repository_services/LoteService.kt
+++ src/main/kotlin/com/acevedosharp/persistence_layer/repository_services/LoteService.kt
@@ -4,7 +4,9 @@ import com.acevedosharp.entities.LoteDB
 import com.acevedosharp.persistence_layer.repositories.LoteRepo
 import com.acevedosharp.persistence_layer.repository_services.interfaces.BatchSaving
 import com.acevedosharp.persistence_layer.repository_services.interfaces.NonEditableRepoService
+import org.springframework.stereotype.Service
 
+@Service
 class LoteService(val repo: LoteRepo): NonEditableRepoService<LoteDB>,
     BatchSaving<LoteDB> {
     override fun all(): List<LoteDB> = repo.findAll()
